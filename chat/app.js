42593935b224c8305de30351d7d8157d39ed2189
hopga123416 <hopga123416@gmail.com>
2016-05-30 17:54:12 +0700

    chat

---
+++ chat/app.js
@@ -0,0 +1,39 @@
+var express = require('express')
+  , app = express()
+  , http = require('http')
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server);
+
+server.listen(8080);
+
+// routing
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+var usernames = {};
+
+io.sockets.on('connection', function (socket) {
+
+  socket.on('sendchat', function (data) {
+    io.sockets.emit('updatechat', socket.username, data);
+  });
+  socket.on('sendimage', function (data) {
+    io.sockets.emit('updateimage', socket.username, data);
+  });
+
+  socket.on('adduser', function(username){
+
+    socket.username = username;
+    usernames[username] = username;
+    socket.emit('updatechat', 'SERVER', 'you have connected');
+    socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+    io.sockets.emit('updateusers', usernames);
+  });
+
+  socket.on('disconnect', function(){
+    delete usernames[socket.username];
+    io.sockets.emit('updateusers', usernames);
+    socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+  });
+});
